753b467c623d9d57c8005f19c82c1e57cbeada7d
jb-network <85036487+jb-network@users.noreply.github.com>
2022-06-08 01:08:20 -0500

    Add project files.

---
+++ The_Point/Program.cs
@@ -0,0 +1,8 @@
+﻿/* This is challenge work for the "C# Players Guide"
+Level 24 "The Point" Challenge
+Goals:
+1) Create a point class to store a point in two dimensions
+2) Each point is represented by an X coordinate (left/right from an origin point)
+3) Each point is represented by a Y coordinate (Up/down from an origin point)
+*/
+
