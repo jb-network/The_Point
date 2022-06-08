1dec7383a31fc0280ee76c294aabc80a8966b6e3
jb-network <85036487+jb-network@users.noreply.github.com>
2022-06-08 01:37:40 -0500

    Update Program.cs

--- The_Point/Program.cs
+++ The_Point/Program.cs
@@ -4,11 +4,11 @@ Goals:
 1) Create a point class to store a point in two dimensions
 2) Each point is represented by an X coordinate (left/right from an origin point)
 3) Each point is represented by a Y coordinate (Up/down from an origin point)
+Note: Added point_3 to demonstrate that the parameterless constructor works
 */
 
 Point point_1 = new Point(2, 3);
 Point point_2 = new Point(-4,0);
-//Testing parameterless constructor
 Point point_3 = new Point();
 
 Console.WriteLine($"Point 1: ({point_1.x_Coord}, {point_1.y_Coord})");
